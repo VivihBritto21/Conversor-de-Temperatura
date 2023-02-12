8c9eb57edf277f4655ab4ca5bb8227fd708ae5fb
VivihBritto21 <vivianebritope@hotmail.com>
2023-02-11 22:19:27 -0300

    envio de arquivos de projeto

---
+++ setup.js
@@ -0,0 +1,21 @@
+var nome;
+nome = prompt("Bem vinda, por gentileza qual seu nome?");
+var Fahrenheit = -17.22;
+var vivih = -272.15;
+valorCelsius = prompt("Quantos graus celsius quer converter?");
+var resultadoFahrenheit = valorCelsius * 1.8 + 32;
+var resultadovivih = valorCelsius + 273.15;
+alert(
+  nome +
+    " A temperatura de " +
+    valorCelsius +
+    " graus Celsius em Fahrenheit e de " +
+    resultadoFahrenheit
+);
+alert(
+  nome +
+    " A temperatura de " +
+    valorCelsius +
+    " graus Celsius em Vivih e de " +
+    resultadovivih
+);
